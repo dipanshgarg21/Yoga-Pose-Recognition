34bc7bb00077d6bb71cf5b59158a2517f251954d
dipanshgarg21 <dipanshgarg21@gmail.com>
2023-05-01 21:54:06 +0530

    Yoga

---
+++ Yoga/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+import Yoga from './pages/Yoga/Yoga'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Yoga />} />
+      </Routes>
+    </Router>
+  )
+}
+
+
